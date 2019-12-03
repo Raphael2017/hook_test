025bd24a4bf50c6324b3adbb2bb431cddac9ff40
qwerty <2010301140007@whu.edu.cn>
2019-12-03 12:54:10 +0800

    cm

--- hdb_new/src/hdb_new.cpp
+++ hdb_new/src/hdb_new.cpp
@@ -37,7 +37,8 @@ SQLDBC_Retcode prepare_new(SQLDBC::SQLDBC_PreparedStatement *self,
                            const SQLDBC_StringEncoding encoding) {
     SQLDBC_Retcode ret = SQLDBC_NOT_OK;
 
-    wprintf(L"Call prepare_new: %s\n", (wchar_t*)sql);
+    std::wstring tmp = (wchar_t*)sql;
+    wprintf(L"Call prepare_new: %S\n", tmp.c_str());
 
     g_thePreparedStatementMgr->AddedStatementSQL(self, sql, encoding);
 
